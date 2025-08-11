55042bbf2b94bf4b9179fc985ca1be233576bd0a
Ar5h1a-M <2542915@students.wits.ac.za>
2025-08-11 20:09:59 +0200

    DB set up attempt

--- API/server.js
+++ API/server.js
@@ -1,1 +1,10 @@
+const db = require("./db");
 
+(async () => {
+  try {
+    const [rows] = await db.query('SELECT NOW() AS currentTime');
+    console.log('Database connected! Time:', rows[0].currentTime);
+  } catch (err) {
+    console.error('Database connection failed:', err);
+  }
+})();
